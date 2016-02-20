ffc495947e34b68b23096f4cdedf0a9c664b7e27
Imogen <imogen.wentworth@gmail.com>
2016-02-19 21:55:11 -0800

    Add demo page

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,39 @@
-var canvas = document.getElementById("demo-canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+var Demo = (function() {
+  var canvas = document.getElementById("demo-canvas");
+  var buttons = document.querySelector(".buttons");
+  var defaultBackground = "checkerboard";
 
-// html5_canvas_backgrounds.checkerboard.draw({ id: "demo-canvas" });
-// html5_canvas_backgrounds.polkadot.draw({ id: "demo-canvas" });
-html5_canvas_backgrounds.mosaic.draw({ id: "demo-canvas" });
+  return {
+    setCanvasSize: function() {
+      canvas.width = window.innerWidth;
+      canvas.height = 660;
+    },
+    setCanvasBackground: function(background) {
+      html5_canvas_backgrounds[background].draw({ id: "demo-canvas" });
+    },
+    setLogo: function(background) {
+      var logos = document.getElementsByClassName("logo");
+
+      Array.prototype.forEach.call(logos, function(logo) {
+        if (Array.prototype.indexOf.call(logo.classList, background) > -1) {
+          logo.style.display = "block";
+        } else {
+          logo.style.display = "none";
+        }
+      });
+    },
+    run: function() {
+      this.setCanvasSize();
+      this.setCanvasBackground(defaultBackground);
+      this.setLogo(defaultBackground);
+
+      buttons.addEventListener("click", function(e) {
+        e.preventDefault();
+        this.setCanvasBackground(e.target.id);
+        this.setLogo(e.target.id);
+      }.bind(this));
+    }
+  }
+})();
+
+Demo.run();
