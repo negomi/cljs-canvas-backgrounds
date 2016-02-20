2d4ea9f52af7b6c62987b081f3eb0d22dcac9ede
Imogen <imogen.wentworth@gmail.com>
2016-02-19 16:24:02 -0800

    Add mosaic

--- js/demo.js
+++ js/demo.js
@@ -3,4 +3,5 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // html5_canvas_backgrounds.checkerboard.draw({ id: "demo-canvas" });
-html5_canvas_backgrounds.polkadot.draw({ id: "demo-canvas" });
+// html5_canvas_backgrounds.polkadot.draw({ id: "demo-canvas" });
+html5_canvas_backgrounds.mosaic.draw({ id: "demo-canvas" });
