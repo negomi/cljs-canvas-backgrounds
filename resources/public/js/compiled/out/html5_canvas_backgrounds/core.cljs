0d4a5180f80b482b83b4ac2a774ccb81a8b73408
Imogen <imogen.wentworth@gmail.com>
2016-02-26 15:17:11 -0800

    Add production build

--- resources/public/js/compiled/out/html5_canvas_backgrounds/core.cljs
+++
@@ -1,5 +0,0 @@
-(ns html5-canvas-backgrounds.core
-  (:require html5-canvas-backgrounds.checkerboard
-            html5-canvas-backgrounds.polkadot
-            html5-canvas-backgrounds.mosaic
-            html5-canvas-backgrounds.snowfall))
--- resources/public/js/compiled/out/html5_canvas_backgrounds/core.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.7.170 {}
-goog.provide('html5_canvas_backgrounds.core');
-goog.require('cljs.core');
-goog.require('html5_canvas_backgrounds.checkerboard');
-goog.require('html5_canvas_backgrounds.polkadot');
-goog.require('html5_canvas_backgrounds.mosaic');
-goog.require('html5_canvas_backgrounds.snowfall');
-
-//# sourceMappingURL=core.js.map?rel=1456175393096
